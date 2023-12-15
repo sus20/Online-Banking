df20bf7168701596b62a516a26f4da966a53f06d
Himal Puri <purihimal9@gmail.com>
2023-12-15 16:58:45 +0100

    feat: added-bare-metal-CRUD

--- notification-service/main.py
+++ notification-service/main.py
@@ -5,6 +5,8 @@ import uvicorn
 app = FastAPI()
 port = int(os.getenv("PORT-NOTIFICATION", 8001))
 
+to_be_implemented = {"message": "To be implemented"}
+
 @app.get("/ping")
 def ping():
     return {"status": "active"}
@@ -12,6 +14,20 @@ def ping():
 @app.get("/")
 def ping():
     return {"status": "active"}
+
+@app.get("/notifications")
+def get_notifications():
+    return to_be_implemented
+
+# Get Specific Notification
+@app.get("/notifications/{notification_id}")
+def get_specific_notification(notification_id: int):
+    return to_be_implemented
+
+# Post Notification
+@app.post("/notifications")
+def post_notification(message: str):
+    return to_be_implemented
 
 if __name__ == "__main__":    
     uvicorn.run(app, host="0.0.0.0", port=port)
--- transaction-service/main.py
+++ transaction-service/main.py
@@ -5,6 +5,8 @@ import uvicorn
 app = FastAPI()
 port = int(os.getenv("PORT-TRANSACTION", 8002))
 
+to_be_implemented = {"message": "To be implemented"}
+
 @app.get("/ping")
 def ping():
     return {"status": "active"}
@@ -12,6 +14,18 @@ def ping():
 @app.get("/")
 def ping():
     return {"status": "active"}
+
+@app.post("/transfers/from/{from_account_id}/to/{to_account_id}")
+def transfer_amount(from_account_id: int, to_account_id: int, amount: float):
+    return to_be_implemented
+
+@app.get("/transfers")
+def get_all_transfers():
+    return to_be_implemented
+
+@app.get("/transfers/{transfer_id}")
+def get_specific_transfer(transfer_id: int):
+    return to_be_implemented
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=port)
